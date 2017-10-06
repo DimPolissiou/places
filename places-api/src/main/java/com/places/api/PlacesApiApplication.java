28b823fe672d305d5ffe6b33c09dafec31d41fdd
dimpol <dimpolissiou@gmail.com>
2017-10-06 14:15:01 +0300

    ...and back to WAR

--- places-api/src/main/java/com/places/api/PlacesApiApplication.java
+++ places-api/src/main/java/com/places/api/PlacesApiApplication.java
@@ -2,13 +2,20 @@ package com.places.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
 @EnableOAuth2Client
 @EnableAuthorizationServer
-public class PlacesApiApplication {
+public class PlacesApiApplication extends SpringBootServletInitializer{
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PlacesApiApplication.class);
+    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PlacesApiApplication.class, args);
